11d1e21491fb1c9dcf569ecaf8b5ee068c63c673
Tyler Zarzuela <tyzarzuelamor@gmail.com>
2026-08-30 18:00:16 +0200

    Fix Tailwind CSS directives in global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://tyzarzuela.github.io',
+  integrations: [tailwind()],
 });

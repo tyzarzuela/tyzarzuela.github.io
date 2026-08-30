b9c6cc37e51bdab2de2403ebc55afbefa1560316
Tyler Zarzuela <tyzarzuelamor@gmail.com>
2026-08-30 22:19:56 +0200

    Sheep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://tyzarzuela.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
